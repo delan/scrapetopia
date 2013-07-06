1de56e3424b80f0638e753f0994bdeaafa75759f
Delan Azabani <delan@azabani.com>
2013-07-07 05:32:56 +0800

    fixed minor bugs in download.py

--- download.py
+++ download.py
@@ -1,10 +1,11 @@
 import sys
+import re
 import subprocess
 
 i = 0
 for line in sys.stdin:
 	i += 1
 	url = line.strip()
-	name = re.match('.+/(.+)$').group(0)
-	print '\nFetching file', i + ':', name
-	subprocess.call(['curl', '-#OC-', name])
+	name = re.match('.+/(.+)$', url).group(1)
+	print '\nFetching file', str(i) + ':', name
+	subprocess.call(['curl', '-#OC-', url])
