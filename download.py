f5602c94efdcaf3bd4bcc6a0f66d6a2dc7b7c88f
Delan Azabani <delan@azabani.com>
2013-07-11 13:43:42 +0800

    download.py: followup completing last commit

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import re
 import subprocess
 
@@ -9,17 +10,20 @@ for line in sys.stdin:
 	origurl = url
 	url = re.sub('ftp\.ilectures\.curtin\.edu\.au', '134.7.37.39', url)
 	name = re.match('.+/(.+)$', url).group(1)
-	print '\nFetching file', str(i) + ':', name, '...',
+	print 'Fetching file', str(i) + ':', name, '...',
+	retry = 0
+	wait = 0.1
 	while True:
-		retry = 0
 		retval = subprocess.call(['curl', '-fsOC-', url])
 		if retval in (0, 33):
 			print 'success!'
 			break
 		else:
 			retry += 1
+			time.sleep(wait)
 			print 'error', retval
-			print 'Retry #' + retry, '...',
+			print 'Retry #' + str(retry), '...',
+			wait = (wait * 2) if wait < 15 else 30
 	done = open('done.txt', 'a')
 	done.write(origurl + '\n')
 	done.close()
