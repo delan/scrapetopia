c48fe53efa4208230666c00c05b37bc5adc31310
Delan Azabani <delan@azabani.com>
2013-07-11 13:27:30 +0800

    download.py: record done urls, retry on failures

--- download.py
+++ download.py
@@ -6,7 +6,17 @@ i = 0
 for line in sys.stdin:
 	i += 1
 	url = line.strip()
+	origurl = url
 	url = re.sub('ftp\.ilectures\.curtin\.edu\.au', '134.7.37.39', url)
 	name = re.match('.+/(.+)$', url).group(1)
 	print '\nFetching file', str(i) + ':', name
-	subprocess.call(['curl', '-#OC-', url])
+	while True:
+		retval = subprocess.call(['curl', '-#OC-', url])
+		if retval in (0, 33):
+			print 'Success!'
+			break
+		else:
+			print '\nRetrying, error', retval
+	done = open('done.txt', 'a')
+	done.write(origurl + '\n')
+	done.close()
