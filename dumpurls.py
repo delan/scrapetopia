14e4b46f9b903d710ad60354e9971ca2aac17936
Delan Azabani <delan@azabani.com>
2013-08-27 19:04:54 +0800

    filter out dud URLs from database dumper

--- dumpurls.py
+++ dumpurls.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import os
+import re
 import sqlite3
 
 c = sqlite3.connect(os.path.join('data', 'meta.db'))
 for i in c.execute('SELECT url FROM file;').fetchall():
-	print i[0]
+	if re.match('^http://ftp\.ilectures\.curtin\.edu\.au/', i[0]):
+		print i[0]
